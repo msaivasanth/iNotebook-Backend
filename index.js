b527a955413672e89f67bcf35ddfb1428a599f4a
M. Sai Vasanth <92867026+msaivasanth@users.noreply.github.com>
2023-08-12 12:38:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const app = express()
 const port = 5000
 
 app.use(express.json())
-app.use(corsMiddleware);
-// app.use(cors());
+// app.use(corsMiddleware);
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
