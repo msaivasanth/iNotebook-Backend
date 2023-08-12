76a094a8694c3665a3e0f0ce4b847b9804e58067
M. Sai Vasanth <92867026+msaivasanth@users.noreply.github.com>
2023-08-12 11:35:24 +0530

    Update index.js /

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 5000
 
 app.use(express.json())
 app.use(cors(
-  origin: ["https://i-notebook-frontend-bice.vercel.app"],
+  origin: ["https://i-notebook-frontend-bice.vercel.app/"],
   methods: ["POST", "GET", "PUT", "DELETE"],
   credentials: true
 ))
