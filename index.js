418c678c991f70cde17978478c5474d538f1d5e3
M. Sai Vasanth <92867026+msaivasanth@users.noreply.github.com>
2023-08-12 12:53:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const connectToMongo = require('./db')
 var cors = require('cors')
-const corsMiddleware = require('./corsMiddleware');
+// const corsMiddleware = require('./corsMiddleware');
 
 connectToMongo()
 const app = express()
@@ -9,7 +9,9 @@ const port = 5000
 
 app.use(express.json())
 app.use(corsMiddleware);
-app.use(cors());
+app.use(cors({
+  origin:"*",
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
